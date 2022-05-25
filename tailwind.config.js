b3961c2feda20f3470733db872eaed8b6fec104c
Caleb Cox <canac@users.noreply.github.com>
2022-05-24 20:02:39 -0500

    Initial rewrite

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["src/index.html"],
+  theme: {
+    backgroundImage: {
+      header: "url('/img/header.jpg')",
+    },
+    colors: {
+      black: "#000000",
+      white: "#ffffff",
+      yellow: "#ffcf07",
+      "gray-dark": "#383f43",
+      gray: "#9c9fa1",
+      "gray-light": "#ebecec",
+    },
+    fontFamily: {
+      "source-sans-pro": ["Source Sans Pro", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
